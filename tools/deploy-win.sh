d46492f981d123330eae80ffd5a6fdec85675bbf
lexia lim <limlexia@gmail.com>
2026-06-03 19:51:55 +0900

    Fix desktop release dependency packaging

--- tools/deploy-win.sh
+++ tools/deploy-win.sh
@@ -3,25 +3,33 @@ BINARY=$1
 INSTALLPATH="$2"
 WORKDIR="$3"
 
-
 if [ -z "$DESTDIR" ]; then
 	OUTDIR="$INSTALLPATH"
 else
-	if [ -n $(echo ${INSTALLPATH} | grep "^[A-Z]:") ]; then
+	if echo "$INSTALLPATH" | grep "^[A-Z]:" >/dev/null 2>&1; then
 		INSTALLPATH="${INSTALLPATH:3}"
 	fi
 	OUTDIR="$WORKDIR/$DESTDIR/$INSTALLPATH"
 fi
 
+mkdir -p "$OUTDIR"
+
 IFS=$'\n'
-if [ -n $(which ntldd 2>&1 | grep /ntldd) ]; then
+if which ntldd 2>&1 | grep /ntldd >/dev/null 2>&1; then
 	DLLS=$(ntldd -R "$BINARY" | grep -i mingw | cut -d">" -f2 | sed -e 's/(0x[0-9a-f]\+)//' -e 's/^ \+//' -e 's/ \+$//' -e 's,\\,/,g')
-elif [ -n $(which gdb 2>&1 | grep /gdb) ]; then
+elif which gdb 2>&1 | grep /gdb >/dev/null 2>&1; then
 	DLLS=$(gdb "$BINARY" --command=$(dirname $0)/dlls.gdb | grep -i mingw | cut -d" " -f7- | sed -e 's/^ \+//' -e 's/ \+$//' -e 's,\\,/,g')
 else
 	echo "Please install gdb or ntldd for deploying DLLs"
 fi
-cp -vu $DLLS "$OUTDIR"
-if [ -n $(which windeployqt 2>&1 | grep /windeployqt) ]; then
+if [ -n "$DLLS" ]; then
+	cp -vu $DLLS "$OUTDIR"
+fi
+for MGBA_DLL in "$(dirname "$BINARY")/mgba.dll" "$(dirname "$BINARY")/libmgba.dll"; do
+	if [ -f "$MGBA_DLL" ]; then
+		cp -vu "$MGBA_DLL" "$OUTDIR"
+	fi
+done
+if which windeployqt 2>&1 | grep /windeployqt >/dev/null 2>&1; then
 	windeployqt --no-opengl-sw --no-svg --release --dir "$OUTDIR" "$BINARY"
 fi
